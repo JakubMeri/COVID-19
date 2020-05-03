c589e28a0a76d1a7a1102c669f3c084ada25a699
JM Coding <jakubmeri@gmail.com>
2020-05-03 15:38:35 +0200

    Updates

---
+++ precache-manifest.e0b507e1e2135843018dfe92bd239a5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18642616573c996a8b54b5b48dcf504",
+    "url": "/COVID-19/index.html"
+  },
+  {
+    "revision": "20a3df2bef9b1185fa96",
+    "url": "/COVID-19/static/css/main.c86f05ed.chunk.css"
+  },
+  {
+    "revision": "8dca2331df553355b591",
+    "url": "/COVID-19/static/js/2.e0ccad68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/COVID-19/static/js/2.e0ccad68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a3df2bef9b1185fa96",
+    "url": "/COVID-19/static/js/main.7471c59a.chunk.js"
+  },
+  {
+    "revision": "9d019027bda0f8c496a9",
+    "url": "/COVID-19/static/js/runtime-main.2dee6a49.js"
+  },
+  {
+    "revision": "0d22be24595695817a51ba73baf13f11",
+    "url": "/COVID-19/static/media/hand.0d22be24.jpeg"
+  }
+]);
